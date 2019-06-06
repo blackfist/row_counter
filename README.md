4e4f9d9a25e0b8c80e29a65ae2f52da59ece8378
Kevin Thompson <kevin.david.thompson@gmail.com>
2019-06-06 12:39:09 -0500

    adds diagnostics, documentation, and heroku Procfile

--- README.md
+++ README.md
@@ -3,11 +3,28 @@
 Receives a userId string and an integer value for the number of rows exported by a Salesforce transaction.
 Given a userId it returns an integer that is two standard deviations greater than the mean number of records the user has exported in the past
 
-## Setup
+# Setup
 
 Put the database url into an environment variable named DATABASE_URL. The code will also look for a local file named `.env` to populate the
 database value.
 
-## Running
+Run an interactive python shell with `pipenv shell` and then run this code to create the database
+
+```
+from api import db
+db.create_all()
+```
+
+# Running
 
 `pipenv run gunicorn api:app`
+
+# Deploying to Heroku
+
+From the directory where you've got your source code (make sure you've already logged in to the Heroku CLI)
+
+```
+heroku apps:create
+heroku addons:create heroku-postgresql
+git push heroku master
+```
--- api.py
+++ api.py
@@ -11,17 +11,61 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 # Create our data model
+
+
 class ExportRecord(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.String(18))
     count = db.Column(db.Numeric(1, 6))
 
+
 @app.route("/", methods=["GET"])
 def get_main():
+    print(request.headers)
     return jsonify({
         "status": "ok",
         "message": "Not used. The root endpoint is not used"
     })
 
+
+@app.route("/record", methods=["POST"])
+def store_record_count():
+    print("### HTTP POST received on /record. Header data is as follows")
+    print(request.headers)
+    print("### request.args data received is as follows")
+    print(request.args)
+    print("### request.form data received is as follows")
+    print(request.form)
+    print("### Combined form and args data is as follows")
+    print(request.values)
+    print("### Fallback Data received is as follows")
+    print(request.data)
+    print("### End diagnostic info")
+
+    if request.values.get('userId', 'Missing') == 'Missing':
+        return jsonify({"status": "error", "message": "post data did not contain a userId field"})
+    if request.values.get('count', 'Missing') == 'Missing':
+        return jsonify({"status": "error", "message": "post data did not contain a count field"})
+
+    received_user_id = request.values.get('userId')
+    received_count = request.values.get('count')
+
+    record_count = 0
+    try:
+        record_count = int(received_count)
+    except ValueError:
+        return jsonify({"status": "error",
+                        "message": "could not convert the count value {0} to an integer".format(received_count)})
+
+    new_record = ExportRecord(
+        user_id=received_user_id, count=record_count)
+    db.session.add(new_record)
+    db.session.commit()
+    return jsonify({
+        "status": "successful"
+    })
+
+
+@app.route()
 if __name__ == "__main__":
     app.run()
