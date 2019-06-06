7b4fad3ebf5c0a213704f43402550917d97d32ba
Kevin Thompson <kevin.david.thompson@gmail.com>
2019-06-06 13:23:50 -0500

    adds an endpoint to get the user's max_allowed

--- api.py
+++ api.py
@@ -4,11 +4,19 @@ from flask import request, jsonify
 from dotenv import load_dotenv
 import json
 import os
+import sys
+from statistics import mean, stdev
 
 load_dotenv()
 app = flask.Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 db = SQLAlchemy(app)
+minimum_allowed = os.getenv("MINIMUM_ALLOWED", "10")
+try:
+    minimum_allowed = int(minimum_allowed)
+except ValueError:
+    sys.exit(
+        "Value of environment variable MINIMUM_ALLOWED cannot be converted to an integer")
 
 # Create our data model
 
@@ -63,6 +71,28 @@ def store_record_count():
     db.session.commit()
     return jsonify({
         "status": "successful"
+    })
+
+
+@app.route("/records/<user_id>", methods=['GET'])
+def get_record_boundary(user_id):
+    db_results = ExportRecord.query.filter_by(user_id=user_id).all()
+    raw_counts = [record.count for record in db_results]
+
+    # If the user has not been seen before then there might be no records. So return the minimum allowed
+    if len(raw_counts) == 0:
+        return jsonify({
+            "status": "success",
+            "max_allowed": 10
+        })
+
+    # Calculate the mean and std deviation of this user's previous activity
+    mean = mean(raw_counts)
+    stdev = stdev(raw_counts)
+    ceiling = round(mean + (2*stdev))
+    return jsonify({
+        "status": "success",
+        "max_allowed": ceiling
     })
 
 
